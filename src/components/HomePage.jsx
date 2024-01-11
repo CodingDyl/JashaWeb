2f27a6d39c3214922074a1e3f58dcea24053d079
Dylan Petzer <54238036+CodingDyl@users.noreply.github.com>
2024-01-11 22:55:56 +0200

    responsive fix

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -65,7 +65,7 @@ const HomePage = () => {
         </div>
     </div>
 
-    <div className="absolute xs:bottom-10 md:bottom-20 w-full flex justify-center items-center">
+    <div className="absolute xs:bottom-20 md:bottom-20 w-full flex justify-center items-center">
         <a href={`#about`}>
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-tertiary flex justify-center items-start p-2">
             <motion.div animate={{y: [0, 24, 0]}} transition={{duration: 1.5, repeat: Infinity, repeatType: 'loop' }} className="w-3 h-3 rounded-full bg-tertiary mb-1"/>
