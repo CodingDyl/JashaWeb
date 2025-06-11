f1a0b3d25e8dde577d72001c485d62075a2407a4
Dylan Petzer <54238036+CodingDyl@users.noreply.github.com>
2025-06-11 12:04:08 +0200

    Google issues resolution

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,41 +1,19 @@
 import { Container, Text } from '@mantine/core';
 import { logo_bg } from '../../assets';
 import classes from './Footer.module.css';
-import { Helmet } from 'react-helmet-async';
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <>
-      <Helmet>
-        <script type="application/ld+json">
-          {`
-            {
-              "@context": "https://schema.org",
-              "@type": "Organization",
-              "name": "Jasha Consulting Services",
-              "url": "https://jasha.co.za",
-              "logo": "https://jasha.co.za${logo_bg}",
-              "description": "Industrial Fabrication and Engineering Solutions",
-              "foundingDate": "2005",
-              "address": {
-                "@type": "PostalAddress",
-                "addressCountry": "South Africa"
-              }
-            }
-          `}
-        </script>
-      </Helmet>
-      <footer className={`${classes.footer} mt-5`} role="contentinfo">
-        <Container className={classes.inner}>
-          <img src={logo_bg} alt='Jasha Consulting Services Logo' className='h-10 object-contain' />
-          <Text size="sm" c="dimmed">
-            © {currentYear} Jasha Consulting Services. All rights reserved.
-          </Text>
-        </Container>
-      </footer>
-    </>
+    <footer className={`${classes.footer} mt-5`} role="contentinfo">
+      <Container className={classes.inner}>
+        <img src={logo_bg} alt='Jasha Consulting Services Logo' className='h-10 object-contain' />
+        <Text size="sm" c="dimmed">
+          © {currentYear} Jasha Consulting Services. All rights reserved.
+        </Text>
+      </Container>
+    </footer>
   );
 }
 
